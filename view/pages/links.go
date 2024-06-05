d89e0cccf8d6ceffc9af9143a510a02b245be68d
Mohit Jangra <mohitjangra12@gmail.com>
2024-06-05 22:37:33 +0530

    stores Link objects for link tags

---
+++ view/pages/links.go
@@ -0,0 +1,10 @@
+package pages
+
+import component "github.com/techiemohitjangra/portfolio/view/component"
+
+var cssLink component.Link = component.Link{
+
+	Rel:  "stylesheet",
+	Type: "text/css",
+	Href: "static/css/styles.css",
+}
