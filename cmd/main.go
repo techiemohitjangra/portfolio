e38e3023357f4b37e1be29794a2d09dc4ad552d9
Mohit Jangra <mohitjangra12@gmail.com>
2024-06-14 12:10:11 +0530

    setup echo router and static file path

--- cmd/main.go
+++ cmd/main.go
@@ -4,27 +4,13 @@ import (
 	"log/slog"
 
 	"github.com/labstack/echo/v4"
-	handler "github.com/techiemohitjangra/portfolio/handler"
 )
 
 func main() {
 	router := echo.New()
 	router.Static("/static", "static")
 
-	homeHandler := handler.HomeHandler{}
-	router.GET("/", homeHandler.HandleHomeShow)
-
-	aboutHandler := handler.AboutHandler{}
-	router.GET("/about", aboutHandler.HandleAboutShow)
-
-	resumeHandler := handler.ResumeHandler{}
-	router.GET("/resume", resumeHandler.HandleResumeShow)
-
-	projectsHandler := handler.ProjectsHandler{}
-	router.GET("/projects", projectsHandler.HandleProjectsShow)
-
-	blogsHandler := handler.BlogsHandler{}
-	router.GET("/blogs", blogsHandler.HandleBlogsShow)
+	SetupRoutes(router)
 
 	listenAddr := ":3000"
 	slog.Info("HTTP server started", "listenAddr", listenAddr)
