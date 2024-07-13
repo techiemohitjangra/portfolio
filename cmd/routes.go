f734db406ba3329f0e3acbb4dc13329627d2f685
Mohit Jangra <mohitjangra12@gmail.com>
2024-07-14 00:38:48 +0530

    moved db connection string to .env passed db context to request handler from SetupRouter function rather then initializing it in them also passed environment variables map to the request handlers

--- cmd/routes.go
+++ cmd/routes.go
@@ -1,23 +1,57 @@
 package main
 
 import (
+	"log"
+
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	handler "github.com/techiemohitjangra/portfolio/handler"
+	model "github.com/techiemohitjangra/portfolio/model"
 )
 
 func SetupRoutes(router *echo.Echo) {
-	homeHandler := handler.HomeHandler{}
+	env, err := godotenv.Read(".env")
+
+	db, err := model.OpenDB(env["DB"])
+	if err != nil {
+		log.Println("failed to open test DB: ", err)
+	}
+	defer model.CloseDB(db)
+
+	// handlers to setup the test environment and data
+	setupHandler := handler.TestSetupHandler{
+		DB:  db,
+		Env: env,
+	}
+	router.GET("/setup", setupHandler.SetupHandler)
+
+	// handlers for home page
+	homeHandler := handler.HomeHandler{
+		DB:  db,
+		Env: env,
+	}
 	router.GET("/", homeHandler.HandleHomeShow)
 	router.POST("/contact", homeHandler.HandleContact)
 
-	aboutHandler := handler.AboutHandler{}
+	// handlers for about page
+	aboutHandler := handler.AboutHandler{
+		DB:  db,
+		Env: env,
+	}
 	router.GET("/about", aboutHandler.HandleAboutShow)
-	router.GET("/about/:username", aboutHandler.HandleUserAboutShow)
 
-	resumeHandler := handler.ResumeHandler{}
+	// handlers for resume page
+	resumeHandler := handler.ResumeHandler{
+		DB:  db,
+		Env: env,
+	}
 	router.GET("/resume", resumeHandler.HandleResumeShow)
 
-	projectsHandler := handler.ProjectsHandler{}
+	// handlers for projects
+	projectsHandler := handler.ProjectsHandler{
+		DB:  db,
+		Env: env,
+	}
 	router.GET("/projects", projectsHandler.HandleProjectsShow)
 	router.GET("/project/:id", projectsHandler.HandleProjectShow)
 	router.POST("/project", projectsHandler.HandleProjectCreate)
@@ -27,14 +61,20 @@ func SetupRoutes(router *echo.Echo) {
 	// router.PATCH("/project/content/:id", projectsHandler.HandleProjectsShow)
 	router.DELETE("/project/:id", projectsHandler.HandleProjectDelete)
 
-	blogsHandler := handler.BlogsHandler{}
+	blogsHandler := handler.BlogsHandler{
+		DB:  db,
+		Env: env,
+	}
 	router.GET("/blogs", blogsHandler.HandleBlogsShow)
 	router.GET("/blog/:id", blogsHandler.HandleBlogShow)
 	router.POST("/blog", blogsHandler.HandleBlogCreate)
 	router.PATCH("/project/:id", blogsHandler.HandleBlogUpdate)
 	router.DELETE("/project/:id", blogsHandler.HandleBlogDelete)
 
-	userHandler := handler.UserHandler{}
+	userHandler := handler.UserHandler{
+		DB:  db,
+		Env: env,
+	}
 	router.POST("/register", userHandler.HandleRegister)
 	router.POST("/login", userHandler.HandleLogin)
 	router.POST("/logout", userHandler.HandleLogout)
