634f361342b817aa4338f24e3b4bf274bdaf25ed
Mohit Jangra <mohitjangra12@gmail.com>
2024-07-14 00:40:03 +0530

    moved port to .env and accessed it from .env

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"log"
 	"log/slog"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
@@ -10,9 +12,14 @@ func main() {
 	router := echo.New()
 	router.Static("/static", "static")
 
+	env, err := godotenv.Read(".env")
+	if err != nil {
+		log.Fatal("failed to load .env")
+	}
+
 	SetupRoutes(router)
 
-	listenAddr := ":3000"
+	listenAddr := ":" + env["PORT"]
 	slog.Info("HTTP server started", "listenAddr", listenAddr)
 	router.Start(listenAddr)
 }
