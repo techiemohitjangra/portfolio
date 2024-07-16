19af9020ae4f354e82b2002b5763d41b35b8b618
Mohit Jangra <mohitjangra12@gmail.com>
2024-07-17 01:18:16 +0530

    added link objects for various css files

--- view/pages/links.go
+++ view/pages/links.go
@@ -1,10 +1,61 @@
 package pages
 
-import component "github.com/techiemohitjangra/portfolio/view/component"
+import (
+	component "github.com/techiemohitjangra/portfolio/view/component"
+)
 
-var cssLink component.Link = component.Link{
+var GlobalCSS component.Link = component.Link{
 
 	Rel:  "stylesheet",
 	Type: "text/css",
-	Href: "static/css/styles.css",
+	Href: "static/css/global.css",
+}
+
+var HomeCSS component.Link = component.Link{
+
+	Rel:  "stylesheet",
+	Type: "text/css",
+	Href: "static/css/home.css",
+}
+
+var AboutCSS component.Link = component.Link{
+
+	Rel:  "stylesheet",
+	Type: "text/css",
+	Href: "static/css/about.css",
+}
+
+var resumeCSS component.Link = component.Link{
+
+	Rel:  "stylesheet",
+	Type: "text/css",
+	Href: "static/css/resume.css",
+}
+
+var BlogCSS component.Link = component.Link{
+
+	Rel:  "stylesheet",
+	Type: "text/css",
+	Href: "static/css/blog.css",
+}
+
+var BlogsCSS component.Link = component.Link{
+
+	Rel:  "stylesheet",
+	Type: "text/css",
+	Href: "static/css/blogs.css",
+}
+
+var ProjectCSS component.Link = component.Link{
+
+	Rel:  "stylesheet",
+	Type: "text/css",
+	Href: "static/css/project.css",
+}
+
+var ProjectsCSS component.Link = component.Link{
+
+	Rel:  "stylesheet",
+	Type: "text/css",
+	Href: "static/css/projects.css",
 }
