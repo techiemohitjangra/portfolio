a279d334ee66b96b432ef35e2282d23a3f60888a
Mohit Jangra <mohitjangra12@gmail.com>
2024-08-23 00:08:15 +0530

    added accept header option to aboutHandler

--- handler/AboutHandler.go
+++ handler/AboutHandler.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	model "github.com/techiemohitjangra/portfolio/model"
@@ -27,12 +28,28 @@ func (handler AboutHandler) HandleAboutShow(ctx echo.Context) error {
 		handler.DB = db
 	}
 
-	user, err := model.GetUserShow(handler.DB, handler.Env["DEFAULT_USER"])
-	if err != nil {
-		log.Println("failed to fetch user: ", err)
-		return echo.NewHTTPError(http.StatusBadRequest, map[string]interface{}{
-			"error_message": "failed to fetch user",
-		})
+	acceptHeader := ctx.Request().Header.Get("Accept")
+	if strings.Contains(acceptHeader, "application/json") {
+		user, err := model.GetUserShow(handler.DB, handler.Env["DEFAULT_USER"])
+		if err != nil {
+			log.Println("failed to get UserShow")
+			return ctx.JSON(http.StatusInternalServerError, map[string]interface{}{
+				"error_message": "failed to get UserShow",
+			})
+		}
+
+		return ctx.JSON(http.StatusOK, user)
+	} else if strings.Contains(acceptHeader, "text/html") {
+		user, err := model.GetUserShow(handler.DB, handler.Env["DEFAULT_USER"])
+		if err != nil {
+			log.Println("failed to fetch user: ", err)
+			return echo.NewHTTPError(http.StatusBadRequest, map[string]interface{}{
+				"error_message": "failed to fetch user",
+			})
+		}
+		return render(ctx, pages.AboutPage(user))
 	}
-	return render(ctx, pages.AboutPage(user))
+	return ctx.JSON(http.StatusNotAcceptable, map[string]interface{}{
+		"error_message": "Unsupported format",
+	})
 }
