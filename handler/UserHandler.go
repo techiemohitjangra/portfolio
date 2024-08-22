543330c36ff5eb5900665378913f5418bf56fb97
Mohit Jangra <mohitjangra12@gmail.com>
2024-08-23 00:22:42 +0530

    added logic to register and add user and added boiler plate for other operations

--- handler/UserHandler.go
+++ handler/UserHandler.go
@@ -2,8 +2,11 @@ package handler
 
 import (
 	"database/sql"
+	"log"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
+	model "github.com/techiemohitjangra/portfolio/model"
 )
 
 type UserHandler struct {
@@ -12,13 +15,59 @@ type UserHandler struct {
 }
 
 func (handler UserHandler) HandleRegister(ctx echo.Context) error {
+	if handler.DB == nil {
+		db, err := model.OpenDB(handler.Env["DB"])
+		if err != nil {
+			log.Println("failed to open DB file: ", err)
+			return ctx.JSON(http.StatusInternalServerError, map[string]interface{}{
+				"error_message": "database not available",
+			})
+		}
+		handler.DB = db
+	}
+	var user model.User
+	err := ctx.Bind(user)
+	if err != nil {
+		log.Println("failed to bind user to create user")
+		return ctx.JSON(http.StatusBadRequest, map[string]interface{}{
+			"error_message": "failed to parse user data",
+		})
+	}
+
+	err = model.AddUser(handler.DB, user)
+	if err != nil {
+		log.Println("failed to add user")
+		return ctx.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"error_message": "failed to add user",
+		})
+	}
 	return nil
 }
 
 func (handler UserHandler) HandleLogin(ctx echo.Context) error {
+	if handler.DB == nil {
+		db, err := model.OpenDB(handler.Env["DB"])
+		if err != nil {
+			log.Println("failed to open DB file: ", err)
+			return ctx.JSON(http.StatusInternalServerError, map[string]interface{}{
+				"error_message": "database not available",
+			})
+		}
+		handler.DB = db
+	}
 	return nil
 }
 
 func (handler UserHandler) HandleLogout(ctx echo.Context) error {
+	if handler.DB == nil {
+		db, err := model.OpenDB(handler.Env["DB"])
+		if err != nil {
+			log.Println("failed to open DB file: ", err)
+			return ctx.JSON(http.StatusInternalServerError, map[string]interface{}{
+				"error_message": "database not available",
+			})
+		}
+		handler.DB = db
+	}
 	return nil
 }
